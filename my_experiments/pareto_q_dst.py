d69cc4d4100610a5c613990bb8b90c105a1668cb
Saood1810 <msaood1810@gmail.com>
2024-09-11 22:24:51 +0200

    testing pql log function

--- my_experiments/pareto_q_dst.py
+++ my_experiments/pareto_q_dst.py
@@ -3,11 +3,13 @@ import numpy as np
 import random
 from morl_baselines.multi_policy.pareto_q_learning.pql import PQL
 import os
+import wandb
 
 SEEDS = [42]  # 10 seeds
 env = mo_gym.make("deep-sea-treasure-concave-v0")
 ref_point = np.array([0, -25])
 os.environ['WANDB_MODE'] = 'offline'
+wandb.init(mode="offline",project="Research Project Logs")
 for seed in SEEDS:
     
     print(f"Running experiment with seed {seed}")
@@ -20,15 +22,15 @@ for seed in SEEDS:
         env,
         ref_point,
         gamma=0.9,
-        initial_epsilon=0.1,
+        initial_epsilon=1,
+        epsilon_decay_steps=10000,
         final_epsilon=0.1,
         seed=seed,
-        project_name="Research Project Logs",
         experiment_name="Pareto Q-Learning in DST",
         log=True,)
 
     pf = agent.train(
-        total_timesteps=1000000,
+        total_timesteps=10000,
         log_every=100,
         action_eval="hypervolume",
         known_pareto_front=env.pareto_front(gamma=0.99),
@@ -41,3 +43,4 @@ for seed in SEEDS:
     print(f"Tracking {target}")
     reward = agent.track_policy(target, env=env)
     print(f"Obtained {reward}")
+wandb.finish()
