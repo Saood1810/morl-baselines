2ff13b4d55f10c58ec18a79b585b6d98d59621ae
Saood1810 <msaood1810@gmail.com>
2024-09-26 06:12:55 +0200

    updated pql_dst gamma

--- my_experiments/pql_dst_mirrored.py
+++ my_experiments/pql_dst_mirrored.py
@@ -9,14 +9,14 @@ import wandb
 from utilities import eval_pql,log_results
 
 SEEDS = [42,43,44,45,46,47,48,49,50,51]  # 10 seeds
-env = MORecordEpisodeStatistics(mo_gym.make("deep-sea-treasure-mirrored-v0"), gamma=0.9)
-eval_env = MORecordEpisodeStatistics(mo_gym.make("deep-sea-treasure-mirrored-v0"), gamma=0.9)
+env = MORecordEpisodeStatistics(mo_gym.make("deep-sea-treasure-mirrored-v0"), gamma=0.99)
+eval_env = MORecordEpisodeStatistics(mo_gym.make("deep-sea-treasure-mirrored-v0"), gamma=0.99)
 ref_point = np.array([0, -50])
 
 
 #wandb.init(mode="offline",project="Research Project Logs")
 for seed in SEEDS:
-    wandb.init(mode="offline",project="Research Project Logs V6",group=" Pareto Q-Learning 800k 0.01 in DST Mirrored V2",name="Pareto Q-Learning 0.01 800k in DST Mirrored with seed "+str(seed))
+    wandb.init(mode="offline",project="Research Project Logs V6",group=" PQL gamma 0.99 in DST Mirrored V2",name="Pareto Q-Learning 0.99 in DST Mirrored with seed "+str(seed))
     
     print(f"Running experiment with seed {seed}")
    
@@ -28,9 +28,9 @@ for seed in SEEDS:
     agent = PQL(
         env,
         ref_point,
-        gamma=0.9,
+        gamma=0.99,
         initial_epsilon=1,
-        epsilon_decay_steps=0.01*800000,
+        epsilon_decay_steps=800000,
         final_epsilon=0.1,
         seed=seed,
         experiment_name="Pareto Q-Learning 800k 0.01 in DST mirrored with seed "+str(seed),
@@ -41,7 +41,7 @@ for seed in SEEDS:
         total_timesteps=800000,
         log_every=1000,
         action_eval="hypervolume",
-        known_pareto_front=env.pareto_front(gamma=0.9),
+        known_pareto_front=env.pareto_front(gamma=0.99),
         ref_point=ref_point,
         eval_env=eval_env,)
     
